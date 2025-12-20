566076f7183cd5fc3885e36a59f236fe95e9c71b
messenger1976 <herald_felisilda@yahoo.com>
2025-12-20 14:06:06 +0800

    Update savings edit negative

--- media/js/script/script.js
+++ media/js/script/script.js
@@ -3,34 +3,62 @@
  * and open the template in the editor.
  */
 $(document).ready(function() {
-    var amount = $(".amountformat").val();
-
-    $(".amountformat").val(function(index, value) {
-
-        var x = value.replace(/[^0-9\.]/g, '');
-        var parts = x.toString().split(".");
-        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
-            event.preventDefault();
+    $(".amountformat").on("keypress", function(event) {
+        var currentVal = $(this).val();
+        var cursorPos = this.selectionStart || 0;
+        
+        // Allow arrow keys, backspace, delete, tab, escape, enter
+        if (event.which >= 37 && event.which <= 40 || event.which == 8 || event.which == 46 || event.which == 9 || event.which == 27 || event.which == 13) {
+            return true;
         }
-        return  parts.join(".");
-
-
+        
+        // Allow minus sign only at the beginning
+        if (event.which == 45) {
+            if (cursorPos !== 0 || currentVal.indexOf('-') === 0) {
+                event.preventDefault();
+                return false;
+            }
+            return true;
+        }
+        
+        // Allow decimal point only once
+        if (event.which == 46) {
+            if (currentVal.indexOf('.') != -1) {
+                event.preventDefault();
+                return false;
+            }
+            return true;
+        }
+        
+        // Allow digits (0-9)
+        if (event.which >= 48 && event.which <= 57) {
+            return true;
+        }
+        
+        // Block all other keys
+        event.preventDefault();
+        return false;
     });
 
-    $(".amountformat").on("keypress keyup blur", function(event) {
-        //this.value = this.value.replace(/[^0-9\.]/g,'');
-
-
-        var x = $(this).val().replace(/[^0-9\.]/g, '');
-        var parts = x.toString().split(".");
-        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-        $(this).val(parts.join("."));
-
-        if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
-            event.preventDefault();
+    $(".amountformat").on("keyup blur", function(event) {
+        // Format the value while preserving negative sign
+        var currentVal = $(this).val();
+        var isNegative = currentVal.indexOf('-') === 0;
+        var x = currentVal.replace(/[^0-9\.]/g, '');
+        
+        if (x === '' || x === '.') {
+            $(this).val('');
+            return;
         }
+        
+        var parts = x.toString().split(".");
+        // Only format the integer part (before decimal)
+        if (parts[0] !== '') {
+            parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+        
+        var formatted = (isNegative ? '-' : '') + parts.join(".");
+        $(this).val(formatted);
     });
 
 
